24f9e9eb9fd31d4fd0526d0392c46ae1b40241ab
amiibojeremiah <107762856+amiibojeremiah@users.noreply.github.com>
2024-08-14 10:06:09 -0500

    The Source code

---
+++ Video Idea randomizer/Video Idea Randomizer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Video_Idea_Randomizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(10, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(291, 274);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.Location = new System.Drawing.Point(10, 292);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(294, 44);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(10, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(294, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pick Random";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(10, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(294, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Done";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(10, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(294, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "New";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(316, 491);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Script Idea Picker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Video Idea randomizer/Video Idea Randomizer/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Video_Idea_Randomizer
+{
+    public partial class Form1 : Form
+    {
+        public string thefile;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string folderPath = @"C:\Scripts"; // Change this to your actual folder path
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // Get all text files in the folder
+            string[] files = Directory.GetFiles(folderPath, "*.txt");
+
+            // Check if there are any text files in the folder
+            if (files.Length > 0)
+            {
+                // Generate a random index to pick a file
+                Random random = new Random();
+                int randomIndex = random.Next(files.Length);
+
+                // Load the content of the randomly selected file
+                string selectedFilePath = files[randomIndex];
+                thefile = selectedFilePath;
+                button1.Enabled = true;
+                string fileContent = File.ReadAllText(selectedFilePath);
+
+                // Display the content in the RichTextBox
+                richTextBox1.Text = fileContent;
+                
+            }
+            else
+            {
+                // Optional: Notify the user if no text files are found
+                //MessageBox.Show("No text files found in the folder.");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //save
+            string folderPath = @"C:\Scripts"; // Change this to your actual folder path
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // Count the number of text files in the folder
+            int fileCount = Directory.GetFiles(folderPath, "*.txt").Length;
+
+            // Generate the file name based on the file count
+            string fileName = $"file_{fileCount + 1}.txt";
+            string filePath = Path.Combine(folderPath, fileName);
+
+            // Content to write to the file
+            string content = richTextBox1.Text;
+
+            // Create and write to the file
+            File.WriteAllText(filePath, content);
+
+            button1.Enabled = true;
+
+            // Optional: Notify the user that the file has been saved
+            //MessageBox.Show($"File saved as {fileName}");
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(thefile))
+            {
+                MessageBox.Show("No file selected to move.");
+                return;
+            }
+
+            string destinationFolderPath = @"C:\Scripts\Done"; // Change this to your actual destination folder path
+
+            // Ensure the destination folder exists
+            if (!Directory.Exists(destinationFolderPath))
+            {
+                Directory.CreateDirectory(destinationFolderPath);
+            }
+
+            // Create the full destination path
+            string fileName = Path.GetFileName(thefile);
+            string destinationFilePath = Path.Combine(destinationFolderPath, fileName);
+
+            try
+            {
+                // Move the file
+                File.Move(thefile, destinationFilePath);
+                //MessageBox.Show($"File moved to {destinationFilePath}");
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show($"Error moving file: {ex.Message}");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            thefile = "";
+            button1.Enabled = false;
+        }
+    }
+}
